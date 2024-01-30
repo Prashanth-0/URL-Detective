6fd6034db0ec16a4f4ebb965b9bbfeb858adf439
Prashanth <149233680+Prashanth-0@users.noreply.github.com>
2024-01-30 22:15:18 +0530

    Update URL.py

--- URL.py
+++ URL.py
@@ -1,10 +1,10 @@
 import requests
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
-import whois  # Added for WHOIS lookup
+import whois  
 import colorama
 
-# init the colorama module
+
 colorama.init()
 GREEN = colorama.Fore.GREEN
 GRAY = colorama.Fore.LIGHTBLACK_EX
@@ -35,7 +35,7 @@ def logo(color=RESET):
     {RESET}
     """
 
-# initialize the set of links (unique links)
+
 internal_urls = set()
 external_urls = set()
 
@@ -51,29 +51,29 @@ def get_all_website_links(url):
     Returns all URLs that are found on url in which it belongs to the same
     website
     """
-    # all URLs of url
+    
     urls = set()
-    # domain name of the URL without the protocol
+    
     domain_name = urlparse(url).netloc
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
         if href == "" or href is None:
-            # href empty tag
+            
             continue
-        # join the URL if it's relative (not an absolute link)
+        
         href = urljoin(url, href)
         parsed_href = urlparse(href)
-        # remove URL GET parameters, URL fragments, etc.
+        
         href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
         if not is_valid(href):
-            # not a valid URL
+            
             continue
         if href in internal_urls:
-            # already in the set
+        
             continue
         if domain_name not in href:
-            # external link
+            
             if href not in external_urls:
                 print(f"{GRAY}[!] External link: {href}{RESET}")
                 external_urls.add(href)
@@ -83,7 +83,7 @@ def get_all_website_links(url):
         internal_urls.add(href)
     return urls
 
-# number of URLs visited so far will be stored here
+
 total_urls_visited = 0
 
 def crawl(url, max_urls=30):
